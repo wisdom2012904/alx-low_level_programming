63b477df33c792879fc8f915af532011fb6ae9fb
wisdom2012904 <onyangoodhiambo@gmail.com>
2022-04-08 04:19:40 +0300

    script that prints the size of various variable types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,11 @@
 */
 int main(void)
 {
+printf("size of a char: %d byte(s)\n", sizeof(char));
 printf("size of an int: %d byte(s)\n", sizeof(int));
-printf("size of a long int: %f byte(s)\n", sizeof(long int));
-printf("size of an float: %d byte(s)\n", sizeof(float));
-printf("size of a char: %s byte(s)\n", sizeof(char));
+printf("size of a long int: %d byte(s)\n", sizeof(long int));
 printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("size of an float: %d byte(s)\n", sizeof(float));
 
 return (0);
 }
