254c8c59c0d1d3292443f25de44a74f93482ce80
wisdom2012904 <onyangoodhiambo@gmail.com>
2022-04-08 04:10:10 +0300

    script that prints the size of various variable types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return : Alway 0 (success)
+*/
+int main(void)
+{
+printf("size of an int: %d byte(s)\n", sizeof(int));
+printf("size of a long int: %d byte(s)\n", sizeof(long int));
+printf("size of an float: %d byte(s)\n", sizeof(float));
+printf("size of a char: %d byte(s)\n", sizeof(char));
+printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
+
+return (0)
+}
