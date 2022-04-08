ddee3746d809566cc25d6011efaa33f802650900
wisdom2012904 <onyangoodhiambo@gmail.com>
2022-04-08 03:27:44 +0300

    Script which prints c programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-*Main comment here
+*main - Entry point
+* Return: Always 0 (Success)
 */
-
 int main (void)
 {
-puts("Programming is like building a multilingual puzzle");
-return 0;
+puts("\Programming is like building a multilingual puzzle");
+return (0);
 }
