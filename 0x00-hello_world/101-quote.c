b5da90469170a8268a5b40c8d97cedb286bcbf81
wisdom2012904 <onyangoodhiambo@gmail.com>
2022-04-09 14:25:29 +0300

    Task 8 error fixing

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
 * Return: Always 0 (success)
 */
 int main(void){
-char bap = 'and that peice of art is useful" - Dora korpar, 2015-10-19';
+char bap[] = 'and that peice of art is useful" - Dora korpar, 2015-10-19';
 
 fprintf("%s\n", &bap);
 
