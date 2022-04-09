416c7e6b7232d0d662b57d756cc93e0a475af587
wisdom2012904 <onyangoodhiambo@gmail.com>
2022-04-09 18:10:11 +0300

    Task no. 12 which involve printing combination of two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,7 +9,7 @@ int main(void)
 int num1, num2;
 for (num1 = 0; num1 <= 98; num1++)
 {
-for (num2 = num1 +1; num2 <= 99; num2++)
+for (num2 = num1 + 1; num2 <= 99; num2++)
 {
 putchar((num1 / 10) + '0');
 putchar((num1 % 10) + '0');
@@ -19,6 +19,9 @@ putchar((num2 % 10) + '0');
 
 if (num1 == 98 && num2 == 99)
 continue;
+
+putchar(',');
+putchar(' ');
 }
 }
 
