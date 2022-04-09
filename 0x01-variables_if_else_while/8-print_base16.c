1dca7fe0587f46ee01e8d994dfbff2320e93f53e
wisdom2012904 <onyangoodhiambo@gmail.com>
2022-04-09 12:33:24 +0300

    Task 8 fixing error

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,11 +8,11 @@ int main(void)
 {
 char base16;
 
-for (base16 = 48; base16 <= 57; base16--)
+for (base16 = 48; base16 <= 57; base16++)
 {
 putchar(base16);
 }
-for (base16 = 97; base16 <= 102; base16--)
+for (base16 = 97; base16 <= 102; base16++)
 {
 putchar(base16);
 }
