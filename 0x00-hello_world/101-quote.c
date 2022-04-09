a49c7765b991e499bf5bf829bf931ef1b7ef9c4e
wisdom2012904 <onyangoodhiambo@gmail.com>
2022-04-09 14:34:17 +0300

    Task 8 error fixing

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
 * main - Entry point
 *
 * Return: Always 0 (success)
 */
-int main(void){
-char bap[] = 'and that peice of art is useful" - Dora korpar, 2015-10-19';
-
-fprintf("%s\n", &bap);
+int main(void)
+{
+write(1, "and that peice of art is useful\" - Dora korpar, 2015-10-19\n", 59;
 
 return (1);
 }
