d91bd7e6702076d4051a3ba97ef2de762bdc1b95
wisdom2012904 <onyangoodhiambo@gmail.com>
2022-04-09 12:18:46 +0300

    Task 5 error fixing

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-* main - This is a program prints al single digit number base 10 strating from 0
+* main - print numbers from 0-9
 *
 * Return: Always return 0 (success/correct)
 */
@@ -10,7 +10,7 @@ int m;
 
 for (m = 0; m < 10; m++)
 {
-printf("%d", m);
+printf("%d",m);
 }
 
 return (0);
