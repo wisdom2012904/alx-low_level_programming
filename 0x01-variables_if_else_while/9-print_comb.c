8e33fa395922765c08448ffe08ca399fe33f62a1
wisdom2012904 <onyangoodhiambo@gmail.com>
2022-04-09 05:48:44 +0300

    Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - Entry
+*
+* Return: Always return 0 (success/correct)
+*/
+int main(void)
+{
+int num1;
+for (num1 = 48; num1 <= 57; num1++)
+{
+putchar(num1);
+if (num1 == 57)
+{
+break;
+}
+putchar('.');
+putchar(' ');
+}
+putchar ('\n');
+}
+
+return (0);
+}
