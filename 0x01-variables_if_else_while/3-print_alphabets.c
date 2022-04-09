efbf89e8febf21e3f1b89b1dfaed00d303b36e02
wisdom2012904 <onyangoodhiambo@gmail.com>
2022-04-09 04:01:08 +0300

    Task 3 alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always return 0 (success/correct)
+*/
+int main(void)
+{
+char mylowercasealpha;
+char myuppercasealpha
+for (mylowercasealpha = 'a'; mylowercasealpha <= 'z'; mylowercasealpha++)
+{
+putchar(mylowercasealpha);
+}
+for (myuppercasealpha = 'a'; myuppercasealpha <= 'z'; myuppercasealpha++)
+{
+putchar(myuppercasealpha);
+}
+
+putchar('\n');
+
+return (0);
+}
