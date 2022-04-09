854c7c75d3c999c960fd7be75d5c1a70e21bc098
wisdom2012904 <onyangoodhiambo@gmail.com>
2022-04-09 05:38:13 +0300

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - Entry
+*
+* Return: Always return 0 (success/correct)
+*/
+int main(void)
+{
+char base16;
+
+for (base16 = 48; base16 <= 57; base16--)
+{
+putchar(base16);
+}
+for (base16 = 97; base16 <= 102; base16--)
+putchar(base16);
+}
+putchar ('\n');
+
+return (0);
+}
