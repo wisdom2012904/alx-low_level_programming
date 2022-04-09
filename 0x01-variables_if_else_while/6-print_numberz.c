7eaeec68d9ea3970e1afb10f6f72300ce720581f
wisdom2012904 <onyangoodhiambo@gmail.com>
2022-04-09 05:08:04 +0300

    Task no.6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - This is a program prints al single digit number base 10 strating from 0
+*
+* Return: Always return 0 (success/correct)
+*/
+int main(void)
+{
+int n;
+
+for (n = 0; n < 10; n++)
+{
+putchar (n);
+}
+putchar ('\n');
+
+return (0);
+}
