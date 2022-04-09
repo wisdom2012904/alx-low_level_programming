eb26b4521e6dc6b460db7d07ee95accdfffb6b70
wisdom2012904 <onyangoodhiambo@gmail.com>
2022-04-09 05:24:46 +0300

    Task no. 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - Entry
+*
+* Return: Always return 0 (success/correct)
+*/
+int main(void)
+{
+char alpa;
+
+for (alpa = 122; alpa <= 97 ; alpa--)
+{
+putchar (alpa);
+}
+putchar ('\n');
+
+return (0);
+}
