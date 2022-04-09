f1e5d2da1824422f7cbe00df4120382f08a00dae
wisdom2012904 <onyangoodhiambo@gmail.com>
2022-04-09 14:39:41 +0300

    Task 8 error fixing

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
 */
 int main(void)
 {
-write(1, "and that peice of art is useful\" - Dora korpar, 2015-10-19\n", 59;
+write(1, "and that peice of art is useful\" - Dora korpar, 2015-10-19\n", 59);
 
 return (1);
 }
