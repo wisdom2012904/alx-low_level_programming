eda4fb51bd82a7d0c0fb1ee38739ec6f43b7c84d
wisdom2012904 <onyangoodhiambo@gmail.com>
2022-04-09 03:45:42 +0300

    Task 2 printing lowercase alphabetical letters

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always return 0 (success/correct)
+*/
+int main(void)
+{
+char myalphabet;
+for (myalphabet = 'a'; myalphabet <= 'z'; myalphabet++)
+{
+putchar(myalphabet);
+}
+putchar("\n");
+
+return (0);
+}
