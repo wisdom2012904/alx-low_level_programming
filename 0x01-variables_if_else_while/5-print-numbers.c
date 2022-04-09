ce2c2e10f14d97dbc48206895c952f721ee35d2e
wisdom2012904 <onyangoodhiambo@gmail.com>
2022-04-09 04:47:34 +0300

    Task no. 5 which that prints a single digit number between 0-9

---
+++ 0x01-variables_if_else_while/5-print-numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - This is a program prints al single digit number base 10 strating from 0
+*
+* Return: Always return 0 (success/correct)
+*/
+int main(void)
+{
+int mysingledigit;
+
+for (mysingledigit = 0; mysingledigit < 10; mysingledigit++)
+{
+printf("%i is my sinle digit number\n", mysingledigit);
+}
+return (0);
+}
