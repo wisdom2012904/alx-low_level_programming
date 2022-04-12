9de5e8f6c8f92ba5bd25b85bbba6ce39b5af0264
wisdom2012904 <onyangoodhiambo@gmail.com>
2022-04-12 23:54:40 +0300

    Task 9 error fixing

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,44 +1,47 @@
 #include "main.h"
 /**
-* times_table - a function that print the 9 times table, starting with 0
-*
-* Return: Always 0 
-*/
+ * times_table -prints tables
+ *
+ * Return: Always 0.
+ */
 void times_table(void)
 {
-int i,j,k;
-for (i = 0; i <= 9; i++)
+int a;
+int b;
+int c;
+for (a = 0; a <= 9; a++)
 {
-k = i * j;
-if ((k / 10) == 0)
+for (b = 0; b <= 9; b++)
 {
-if (j == 0)
+c = a * b;
+if ((c / 10) == 0)
 {
-_putchar('0');
+if (b == 0)
+{
+_putchar ('0');
 }
-if (j != 0)
+if (b != 0)
 {
-_putchar(' ');
-_putchar((k / 10) == 0);
+_putchar (' ');
+_putchar ((c % 10) + '0');
 }
-if (j < 9)
+if (b < 9)
 {
 _putchar(',');
-_putchar(' ');
+_putchar (' ');
 }
 }
 else
 {
-_putchar((k / 10) == 0);
-_putchar((k % 10) == 0);
-if (j < 9)
+_putchar ((c / 10) + '0');
+_putchar ((c % 10) + '0');
+if (b < 9)
 {
 _putchar(',');
-_putchar(' ');
+_putchar (' ');
 }
 }
 }
-_putchar('\n');
+_putchar ('\n');
 }
 }
-
