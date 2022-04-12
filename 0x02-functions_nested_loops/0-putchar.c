711582898ffeef9a4003ba26a643df4e15c3e2a6
wisdom2012904 <onyangoodhiambo@gmail.com>
2022-04-12 20:01:42 +0300

    Task 0 error fixing

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,7 @@
 */
 int main(void)
 {
-_putchar('-');
+_putchar('_');
 _putchar('p');
 _putchar('u');
 _putchar('t');
@@ -14,7 +14,8 @@ _putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('r');
-_putchar ('\n');
+
+_putchar('\n');
 
 return (0);
 } 
