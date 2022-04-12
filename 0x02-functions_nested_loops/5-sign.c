236e7bc965d0d53976a935fa4aebb665345c9b31
wisdom2012904 <onyangoodhiambo@gmail.com>
2022-04-12 23:18:22 +0300

    Task 5 error fixing

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,20 +1,29 @@
 #include "main.h"
+
 /**
-* main - a function that prints the sign of a number
-*
-* Return : (1) 
-*/
+ * print_sign - return 0 letter not lowercase, 1 letter lowercase
+ *
+ *@n: the int to print
+ * Return: Always 0.
+ */
 int print_sign(int n)
 {
 if (n > 0)
-_putchar('+);
+{
+_putchar ('+');
 return (1);
+}
 
 else if (n == 0)
-_putchar('0');
+{
+_putchar ('0');
 return (0);
+}
 
-else if (n < 0)
-_putchar('-');
+else
+{
+_putchar ('-');
 return (-1);
-} 
+}
+
+}
