32fac61253d20a2f6d8651231a6b3ae68340e029
wisdom2012904 <onyangoodhiambo@gmail.com>
2022-04-12 21:58:58 +0300

    Task 1 error fixing

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
-* main - a function that prints alphabetica letters in lowercase
+* main - Entry: a function that prints alphabetica letters in lowercase
 *
 * Return : x10 a-z 
 */
 void print_alphabet(void)
 {
 int n;
-for (n = 'a'; n <= 'z'; n++){
+for (n = 'a'; n <= 'z'; n++)
+{
 _putchar(n);
 } 
 _putchar ('\n');
