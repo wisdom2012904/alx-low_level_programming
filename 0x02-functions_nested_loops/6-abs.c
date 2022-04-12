a0049689511db32638b4d9df3f498b0be0c8e04b
wisdom2012904 <onyangoodhiambo@gmail.com>
2022-04-12 23:20:59 +0300

    Task 6 error fixing

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,22 +1,29 @@
 #include "main.h"
+#include <stdio.h>
+
 /**
-* main - a function that computes the absolute value of an integer
-*
-* Return : (0) 
-*/
+ * _abs - value absolute
+ *
+ *
+ * Return: Always 0.
+ */
+
 int _abs(int n)
 {
+
 if (n < 0)
 {
 return (n * (-1));
-return (1);
 }
+
 else if (n == 0)
 {
 return (0);
 }
+
 else
 {
 return (n);
-} 
+}
+
 }
