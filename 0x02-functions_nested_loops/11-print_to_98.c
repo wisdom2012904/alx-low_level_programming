d7fef0d8634ab282f39eecbd6ee9f45110901f6f
wisdom2012904 <onyangoodhiambo@gmail.com>
2022-04-13 10:40:30 +0300

    Task no. 11 error fixing

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,21 +1,22 @@
+#include "main.h"
 #include <stdio.h>
 /**
-* a function that prints all natural numbers from n to 98
-*
-* Return : (0) 
-*/
+ * print_to_98 - print all natural numbers from n to 98.
+ * @n: the number to start counting from n to 98
+ * Return: Always 0.
+ */
 void print_to_98(int n)
 {
-for (n = n; n < 98; n++)
-if (n < 98 )
+if (n < 98)
 {
-printf("%d", n);
+for (n = n; n < 98; n++)
+printf("%d, ", n);
 printf("%d\n", 98);
 }
 else
 {
-for (n = n; n < 98; n--)
-printf("%d", n);
+for (n = n; n > 98; n--)
+printf("%d, ", n);
 printf("%d\n", 98);
 }
 }
