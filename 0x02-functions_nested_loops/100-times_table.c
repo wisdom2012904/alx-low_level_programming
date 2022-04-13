9b00939dbb6c044ea45028c985317ad43e5b6842
wisdom2012904 <onyangoodhiambo@gmail.com>
2022-04-13 10:48:49 +0300

    Task 12 error fixing

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,6 +4,7 @@
  * print_times_table - Prints the times table of the input,
  *                     starting with 0.
  * @n: The value of the times table to be printed.
+ *Return: Always 0
  */
 void print_times_table(int n)
 {
