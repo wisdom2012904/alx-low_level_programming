603d313bb1c0d9975328a08491aa7759ce8a3570
wisdom2012904 <onyangoodhiambo@gmail.com>
2022-04-14 13:33:13 +0300

    Funtion that multiplies two integers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,11 +2,13 @@
 
 /**
  * _isdigit - checks for a digit (0 through 9)
- * @a: first num to be multiply
- * @b: second num to be multiply
+ * @a: first num to be multiply by b
+ * @b: second num to be multiply by a
  * Return: 1 if num is a digit, 0 otherwise
  */
 int mul(int a, int b)
 {
-	return (a * b);
+	int c;
+	c = a * b;
+return (c);
 }
