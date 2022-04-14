8e8f762a09223810b3eff817c1d0f3cad0284f25
wisdom2012904 <onyangoodhiambo@gmail.com>
2022-04-14 19:52:04 +0300

    Task error fixing

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,15 @@
 
 /**
  * print_numbers - prints the numbers, from 0 to 9
+ *
  */
 
 void print_numbers(void)
 {
-	int a;
-
-	for (a = 48; a < 58; a++)
-	{
-		_putchar(a);
-	}
-	_putchar('\n');
+ int a;
+ for (a = 48; a < 58; a++)
+ {
+  _putchar(a);
+ }
+ _putchar('\n');
 }
