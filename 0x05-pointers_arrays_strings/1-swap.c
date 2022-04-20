97dc599ef22ba160287de01e90c159b016b74a27
wisdom2012904 <onyangoodhiambo@gmail.com>
2022-04-20 19:45:24 +0300

    Task 1 which swaps two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the value of two integers, a & b.
+ *
+ * @a: first integer.
+ * @b: second integer.
+ *
+ */
+
+void swap_int(int *a, int *b)
+{
+	int x;
+
+	x = 0;
+	x = *a;
+	*a = *b;
+	*b = c;
+}
