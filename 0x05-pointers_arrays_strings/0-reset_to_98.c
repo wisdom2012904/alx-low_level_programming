4727309c3263ecd987e0d1be6470c3cc8edfbfab
wisdom2012904 <onyangoodhiambo@gmail.com>
2022-04-20 19:35:34 +0300

    Task 0 whic reset the value update *n pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - up the value which *n points to.
- * Description: It updates the value it points to to 98 
+ * Description:It updates the value it points to to 98
  * @n: the parameter to update.
  */
 
