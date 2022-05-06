336977a0f880dd0e297d42acefd4af22d652116a
wisdom2012904 <onyangoodhiambo@gmail.com>
2022-05-06 23:02:38 +0300

    Tasks on advanced projects of Malloc and Free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,21 +3,45 @@
 #include <stdlib.h>
 
 /**
- * free_grid - frees a 2 dimensional grid previously created by
- * alloc_grid function.
- * @grid: matrix double pointer
- * @height: rows (pointer)
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers
+ * @width: columns
+ * @height: rows
  *
- * Return: Nothing
+ * Return: Double Pointer
  */
 
-void free_grid(int **grid, int height)
+int **alloc_grid(int width, int height)
 {
-	int i;
+	int **s;
+	int i, j, k, fr;
 
+	fr = 0;
+	if (width <= 0 || height <= 0)
+		return (0);
+
+	s = malloc(height * sizeof(int *));
+	if (s == 0)
+		return (0);
 	for (i = 0; i < height; i++)
 	{
-		free(*(grid + i));
+		*(s + i) = malloc(width * sizeof(int));
+		if (*(s + i) == 0)
+		{
+			fr = 1;
+			break;
+		}
+		for (j = 0; j < width; j++)
+		{
+			s[i][j] = 0;
+		}
 	}
-	free(grid);
+	if (fr == 1)
+	{
+		for (k = 0; k <= i; k++)
+		{
+			free(*(s + k));
+		}
+		free(s);
+	}
+	return (s);
 }
