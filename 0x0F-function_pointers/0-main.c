41f451f98462c721a9ee6ad7229ef092a7712d08
wisdom2012904 <onyangoodhiambo@gmail.com>
2022-05-17 03:54:21 +0300

    0x0F My commit

--- 0x0F-function_pointers/0-main.c
+++ 0x0F-function_pointers/0-main.c
@@ -45,8 +45,8 @@ void print_name_uppercase(char *name)
  */
 int main(void)
 {
-    print_name("Bob", print_name_as_is);
-    print_name("Bob Dylan", print_name_uppercase);
+    print_name("Godfrey", print_name_as_is);
+    print_name("Godfrey Odhiambo", print_name_uppercase);
     printf("\n");
     return (0);
 }
