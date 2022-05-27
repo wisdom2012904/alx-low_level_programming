45b114009da0c39b4f83be09346dade4d285ca45
wisdom2012904 <onyangoodhiambo@gmail.com>
2022-05-27 14:39:24 +0300

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,15 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <string.h>
-#include <stdio.h>
+
+/*
+ * File: lists.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file containing prototypes and definitions for all functions
+ *       and types written in the 0x12-more_singly_linked_lists directory.
+ */
+
+#include <stdlib.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -31,4 +39,5 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-#endif
+
+#endif /* LISTS_H *
